20a5e735377c038beac5e6bdd35e1579483c6795
mohamed idelaamel <idelaamel@users.noreply.github.com>
2025-08-18 20:00:49 +0100

    update the print line

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ app.post('/webhook', async (req, res) => {
         countryName: attraction.countryName || 'Inconnu',
     }));
 
-    const textResponse = `J'ai trouvé ${attractions.length} attraction(s) à ${city}, de type ${attractionType}.`;
+    const textResponse = `J'ai trouvé ${attractions.length} attraction(s) ${attractionType ? `de type ${attractionType}` : ''} à ${city}.`;
 
     // --- 6. Respond to Dialogflow (same as before) ---
     res.json({
